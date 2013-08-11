faa8b3937429b0f0d07bee901dd4745c8a88097b
Ranjith D <ranjithdharmaraj@yahoo.com>
2013-08-12 00:18:33 +1000

    Correct robot_spec which had the full tests

--- rspec/robot_spec.rb
+++ rspec/robot_spec.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 $LOAD_PATH.unshift File.expand_path('../',__FILE__)
 
@@ -13,6 +13,19 @@ describe 'Robot Simulator' do
          simulator.y.should eq 0
          simulator.f.should eq 'NORTH'
       end
+
+      it 'should move one unit forward from default position' do
+         place_robot
+         simulator.move_robot
+         simulator.x.should eq 0
+         simulator.y.should eq 1
+         simulator.f.should eq 'NORTH'
+      end
+
+      it 'should output the co-ordinates' do
+         place_robot
+         simulator.report.should eq "Robot Co-ordinates => 0,0,NORTH"
+      end
  
       it 'should change directions on command' do
          place_robot
@@ -23,22 +36,34 @@ describe 'Robot Simulator' do
          simulator.report.should eq "Robot Co-ordinates => 0,1,NORTH"
       end          
     
-      it 'should move one unit forward from default position' do
-         place_robot
-         simulator.move_robot
-         simulator.x.should eq 0
-         simulator.y.should eq 1
-         simulator.f.should eq 'NORTH'
+      it 'show correct direction facing when turned right' do
+	 simulator.place(2,2,'NORTH')
+         simulator.change_direction('RIGHT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,EAST"
+         simulator.change_direction('RIGHT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,SOUTH"
+         simulator.change_direction('RIGHT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,WEST"
+         simulator.change_direction('RIGHT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,NORTH"
       end
-    
-      it 'should output the co-ordinates' do
-         place_robot
-         simulator.report.should eq "Robot Co-ordinates => 0,0,NORTH"
+
+      it 'show correct direction facing when turned left' do
+         simulator.place(2,2,'NORTH')
+         simulator.change_direction('LEFT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,WEST"
+         simulator.change_direction('LEFT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,SOUTH"
+         simulator.change_direction('LEFT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,EAST"
+         simulator.change_direction('LEFT')
+         simulator.report.should eq "Robot Co-ordinates => 2,2,NORTH"
       end
-    
+
       it 'should ignore faulty commands' do
          place_robot
          simulator.place(4,4,'NORTH')
+         simulator.move_robot 
          simulator.change_direction('RIGHT')
          simulator.report.should eq 'Robot Co-ordinates => 4,4,EAST'
          simulator.move_robot
@@ -53,6 +78,18 @@ describe 'Robot Simulator' do
          simulator.place(4,0,'EAST')
          simulator.move_robot
          simulator.report.should eq 'Robot Co-ordinates => 4,0,EAST'
+
+         simulator.place(0,4,'NORTH')
+         simulator.move_robot
+         simulator.report.should eq 'Robot Co-ordinates => 0,4,NORTH'
+
+         simulator.place(4,2,'EAST')
+         simulator.move_robot
+         simulator.report.should eq 'Robot Co-ordinates => 4,2,EAST'
+
+         simulator.place(2,0,'SOUTH')
+         simulator.move_robot
+         simulator.report.should eq 'Robot Co-ordinates => 2,0,SOUTH'
       end
    end
 end
